1cb8b7867947dcf40b6470dbad5490ff572ebe96
Devdatta Kulkarni <devdatta.kulkarni@rackspace.com>
2016-05-19 10:54:15 -0500

    Adding solum-app-build.sh

---
+++ solum-app-build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Check if pip is installed
+pip help
+[[ $? != 0 ]] && echo python-pip is not installed. && exit 1
+
+# Install dependencies from app requirements file
+pip install -r requirements.txt
